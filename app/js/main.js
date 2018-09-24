f023bf8c3bfd5d4df8553130d1deb8011a60d127
idan <idan.avin@gmail.com>
2018-09-24 20:10:42 +0300

    init

--- app/js/main.js
+++ app/js/main.js
@@ -1,39 +1,42 @@
 var turn = 0;
-var ticToe = [ , ];
-var board = [];
+var ticToe = [,];
+var board = ['', '', '', '', '', '', '', '', ''];
+var player = 'x';
 
-board[0] = new Array(3);
-board[1] = new Array(3);
-board[2] = new Array(3);
-
-function whosTurn() {  
-
+function checkMoves() {
+    var isClicked = document.querySelectorAll('h');
+    if (isClicked > 0) {
+        checkWin();
+    }
+    else {
+        document.getElementById('winner').innerHTML = 'Draw';
+    }
 }
 
-window.onload = function () {  
+window.onload = function () {
 
     var boxes = document.querySelectorAll('li');
-
-    for(var i = 0; i < boxes.length; i++) {
-        boxes[i].addEventListener('click', function () {  
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener('click', function () {
+            if (turn === 0) { player = 'x'; turn = 1; }
+            else { player = 'o'; turn = 0; }
+            board.splice(boxes[i], 1, player); //boexes[i] = 0?
+            console.log(board);
             
+            $(this).replaceWith('<h class="clicked" id="box-' + player + '">' + player + '</h>');
+            //checkMoves();
         });
     }
 
-    var box = document.getElementById('gBoard');
-    var boxs = document.getElementsByClassName('grid');
-    boxs.addEventListener('click', function (turn) {
-        var clicked = document.createElement('img');
-        clicked.src = ticToe[turn];
-        clicked.id = turn;
-        
-        // this.src = ticToe[turn]
-        turn = (turn + 1) % 1
-        console.log(turn);
-        
-    });
-    for (var index = 0; index > 9; index++) {
+    // var box = document.getElementById('gBoard');
+    // var boxs = document.getElementsByClassName('grid');
+    // boxes.addEventListener('click', function (turn) {
+    //     turn = (turn + 1) % 1
+    //     console.log(turn);
 
-    }
+    // });
+    // for (var index = 0; index > 9; index++) {
+
+    // }
 
 }
