1a807e1950c62c85b74226f95a73a0a00e6147f3
idan <idan.avin@gmail.com>
2020-01-19 00:29:48 +0200

    update

--- new/main.js
+++ new/main.js
@@ -1,39 +1,40 @@
 let board;
-let active;
 let winner = null;
+let active;
 const players = ['X', 'O'];
 const colors = ['blue', 'red', 'green', 'pink'];
-let width;
-let height;
-let w;
-let h;
 let canv = document.createElement("canvas");
 document.body.append(canv);
 let ctx = canv.getContext('2d');
 
-function drawBoard(el) {
-    width = el.width;
-    height = el.height;
-    w = width / 3;
-    h = height / 3;
+function drawBoard() {
+    canvSize = getSize(canv);
 
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
-            ctx.strokeRect(i * w, j * h, (i * w) + w, (j * h) + h);
+            ctx.strokeRect(i * canvSize.w, j * canvSize.h, (i * canvSize.w) + canvSize.w, (j * canvSize.h) + canvSize.h);
         }
     }
 }
+function getSize(el) {
+    let width = el.width;
+    let height = el.height;
+    let w = width / 3;
+    let h = height / 3;
+    return {width, height, w, h}
+}
 
 function play(mousex, mousey) {
+    let canvSize = getSize(canv);
     let played = false;
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
-            if (mousex > i * w && mousex < (i * w) + w) {
-                if (mousey > j * h && mousey < (j * h) + h) {
+            if (mousex > i * canvSize.w && mousex < (i * canvSize.w) + canvSize.w) {
+                if (mousey > j * canvSize.h && mousey < (j * canvSize.h) + canvSize.h) {
                     if (!board[i][j]) {
                         board[i].splice(j, 1, players[active]);
                         ctx.fillStyle = colors[active];
-                        ctx.fillText(players[active], (i * w) + (w / 2), (j * h) + (h / 2), w);
+                        ctx.fillText(players[active], (i * canvSize.w) + (canvSize.w / 2), (j * canvSize.h) + (canvSize.h / 2), canvSize.w);
                         played = true;
                     }
                 }
@@ -61,7 +62,7 @@ function checkWinner() {
             winner = players[active];
         }
     }
-    //Spots taken
+    //check for spots taken
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             if (board[i][j]) {
@@ -75,6 +76,7 @@ function checkWinner() {
 }
 
 function endGame(score) {
+    let canvSize = getSize(canv);
     let endText = '';
     if (score === 'tie') {
         endText = 'Its a tie';
@@ -84,10 +86,10 @@ function endGame(score) {
     }
     ctx.font = 'bold 72px sans-serif';
     ctx.fillStyle = colors[3];
-    ctx.fillRect(w / 2, h / 2, 2 * w, 2 * h);
+    ctx.fillRect(canvSize.w / 2, canvSize.h / 2, 2 * canvSize.w, 2 * canvSize.h);
     ctx.fillStyle = colors[2];
-    ctx.fillText(endText, width / 2, height / 2 - 100);
-    ctx.fillText(`Play again?`, width / 2, height / 2 + 100);
+    ctx.fillText(endText, canvSize.width / 2, canvSize.height / 2 - 100);
+    ctx.fillText(`Play again?`, canvSize.width / 2, canvSize.height / 2 + 100);
     canv.removeEventListener('click', getMousePos);
     canv.addEventListener('click', newGame);
     winner = null;
@@ -120,14 +122,15 @@ function newGame() {
         ['', '', '']
     ];
     active = 0;
-    ctx.clearRect(0, 0, width, height);
+    let canvSize = getSize(canv)
+    ctx.clearRect(0, 0, canvSize.width, canvSize.height);
     //Reset canvas to css size
     canv.width = canv.clientWidth;
     canv.height = canv.clientHeight;
     ctx.font = 'bold 48px sans-serif';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
-    drawBoard(canv);
+    drawBoard();
     canv.removeEventListener('click', newGame)
     canv.addEventListener('click', getMousePos);
 }
